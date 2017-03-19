5fd7cbc878a5e455a4a20258486bc00b6f248177
aplchian <alex.boquis@gmail.com>
2017-03-19 00:20:03 -0400

    added routing

--- shila-web/src/App.js
+++ shila-web/src/App.js
@@ -13,19 +13,34 @@ import {
 } from './pages'
 import { Nav } from './components'
 
+var addBtnStyle = {
+  background: `url('./add-btn.png') no-repeat center center`,
+  height: 13,
+  width: 13,
+  backgroundSize: '13px 13px',
+  position: 'absolute',
+  top: 17,
+  right: 21
+}
 
 class App extends Component {
   render() {
     return (
         <BrowserRouter>
           <div>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={AddPractice}/>
-            <Route path="/about" component={AddSession}/>  
+          <nav className="nav-bar">
+            <h1 className="logo tc ttu">Shila</h1>
+              <Link to="/session/add">
+                <div className="nav-add" style={addBtnStyle} ></div>
+              </Link>
+          </nav>
+          <Route exact path="/" component={Home}/>
+          <Route path="/practice/add" component={AddPractice}/>
+          <Route path="/session/add" component={AddSession}/>  
           </div>       
         </BrowserRouter>
     );
   }
 }
 
-export default Nav(App)
+export default App
--- shila-web/src/components/Nav.js
+++ shila-web/src/components/Nav.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link, BrowserRouter as Router } from 'react-router-dom'
+
+
+var addBtnStyle = {
+  background: `url('./add-btn.png') no-repeat center center`,
+  height: 13,
+  width: 13,
+  backgroundSize: '13px 13px',
+  position: 'absolute',
+  top: 17,
+  right: 21
+}
+
 
 module.exports = Component => () => (
-  <div>
-    <h1>Shiwa</h1>
-    <Component />
-  </div>
+  <Router>
+    <div>
+      <nav className="nav-bar">
+        <h1 className="logo tc ttu">Shila</h1>
+          <Link to={`session/add`}>
+            <div className="nav-add" style={addBtnStyle} ></div>
+          </Link>
+      </nav>
+      <Component />
+    </div>
+  </Router>
 )

77e346e4eb255626b9cf9e3ba4ee342493a11e29
aplchian <alex.boquis@gmail.com>
2017-04-01 11:43:43 -0400

    home page functionality working

--- shila-web/src/components/home-cursor.js
+++ shila-web/src/components/home-cursor.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import { append, init, length, find, filter, join,
+         indexOf, head, toUpper, isEmpty, prop, type } from 'ramda'
+
 
 const styles = {
   background: '#494949',
@@ -10,28 +13,84 @@ const styles = {
 module.exports = React.createClass({
   getInitialState(){
     return({
-      show: true
+      show: true,
+      practices: [
+        {
+          "_id": "practice_d32b5acb-0386-414c-8a8c-794d4daf5a36",
+          "_rev": "1-9bf0a10c296ba430710af07deb111744",
+          "format": "duration",
+          "name": "Meditation",
+          "type": "practice"
+        },
+        {
+          "_id": "practice_9389892c-fa03-4ad8-9374-f8ec15b59160",
+          "_rev": "1-eeb9fa0934017c6a18044cd16df8c241",
+          "format": "count",
+          "name": "Tara",
+          "type": "practice"
+        }
+      ]
     })
   },
   blink(){
     this.setState({show: !this.state.show})
   },
+  onKeyUp(e){
+    if(length(this.state.value) > 1){
+     const filterPractices = ({ name }) => toUpper(name).indexOf(toUpper(this.state.value), 0) > -1
+     const filtered = filter(filterPractices,this.state.practices)
+     const found = !!filtered ? head(filtered) : []
+     return this.setState({found})
+    }else {
+      return this.setState({found: []})
+    }
+  },
+  onSubmit(e){
+    if(type(this.state.found === 'Object')){
+      console.log(`${this.state.found.name} Selected!`)
+    }
+  },
+  onKeyPress(key){
+  const value = join('',append(key,this.state.value))
+  const suggested = find
+  return this.setState({value})
+  },
   componentDidMount(){
     this.timer = setInterval(this.blink, 700);
+    document.addEventListener('keydown',({key}) => {
+      if(key === 'Backspace'){
+        const value = init(this.state.value)
+        return this.setState({value})
+      }else if(key === 'Enter'){
+        return this.onSubmit()
+      }else if (length(key) === 1){
+        this.onKeyPress(key)
+      }
+    })
+
+    document.addEventListener('keyup',({key}) => {
+      this.onKeyUp(key)
+    })
+
   },
   componentWillUnmount: function(){
     clearInterval(this.timer);
   },
   render(){
     const { text } = this.props
+    const suggestion = type(this.state.found) === 'Object'
+                         ? prop('name',this.state.found) 
+                         : null
+
     return(
       <div className="home-cursor-container">
-       <span className="home-cursor-text">{text}</span> 
+       <span className="home-cursor-text">{this.state.value}</span> 
         {
           this.state.show 
             ? <div className="dib" style={styles}></div>
             : null
         }
+        <span className="home-suggestion-text">{suggestion}</span>
       </div>
     )
   }
--- shila-web/src/pages/home.js
+++ shila-web/src/pages/home.js
@@ -10,39 +10,10 @@ import moment from 'moment'
 module.exports = React.createClass({
   getInitialState(){
     return({
-      data: [],
-      sortOptions,
-      currentOption: head(sortOptions)
+      data: []
     })
   },
-  componentDidMount(){
-    getAllSessions()
-      .then((obj) => {
-        const objKeys = keys(obj)
-        const data = map((key) => ({name: key, count: obj[key]}),objKeys)
-        this.setState({data})
-      })
-      .catch(err => console.log('err',err))
-  },
-  handleDateClick(date){
-    return e => {
-      console.log('date',date)
-    }
-  },
   render(){
-    const renderTimes = ({ name, count }) => {
-      return (
-        <li className="w-100 home-list cf mt2">
-          <Link className="link" to={`/session/add/${name}`}>
-            <div className="fl link white">{name}</div>
-          </Link>
-          <div className="fr">{count}</div>
-        </li>
-      )
-    }
-
-    console.log('state',this.state)
-
     return (
       <div className="mt4">
         <HomeCursor />
@@ -50,24 +21,3 @@ module.exports = React.createClass({
     )
   }
 })
-
-
-
-var sortOptions = [
-  {
-    value: 'today',
-    name: 'today'
-  },
-  {
-    value: 'all-time',
-    name: 'all time'
-  },
-  {
-    value: 'last-7-days',
-    name: 'last 7 days'
-  },
-  {
-    value: 'this-month',
-    name: 'this month'
-  },
-]
